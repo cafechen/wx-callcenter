aa79145a31c4a11868557a4de6065ce568022cb1
Shawn Zhu <davidzhu.4.2@gmail.com>
2013-09-08 17:39:40 +0800

    provide helper for handling messages

---
+++ test/msg_helper_test.js
@@ -0,0 +1,62 @@
+var should = require('should');
+
+var helper = require('../lib/msg_helpers');
+
+describe('manage messages', function () {
+  var mockMsgId = "foo",
+    mockMsgReceipt = "mock receipt",
+    mockUserId = "bar",
+    mockMsgBody = "hi";
+
+  it('post sent message', function (done) {
+    helper.sentMsg(mockUserId, mockMsgId, mockMsgReceipt, function (err, data) {
+      should.not.exist(err);
+      //console.log(data);
+      done();
+    });
+  });
+
+  it('append message', function (done) {
+    var isAppended_1 = helper.appendMsg(mockMsgId, mockMsgBody);
+    isAppended_1.should.be.ok;
+
+    //var isAppended_2 = helper.appendMsg(mockMsgId, mockMsgBody);
+    //isAppendend_2.should.be.ok;
+
+    done();
+  });
+
+  it('read message', function (done) {
+    helper.readMsgs(mockUserId, function (data) {
+      data.should.have.property("length");
+      data.should.include(mockMsgBody);
+      done();
+    });
+  });
+
+  it('reads no message', function (done) {
+    helper.readMsgs('non-exist-user-id', function (data) {
+      data.length.should.eql(0);
+      done();
+    });
+  });
+
+  it('clean message', function (done) {
+    helper.cleanupMsgs(mockMsgId, function (data) {
+      data.should.have.property("length");
+      data.length.should.eql(1);
+      data[0].should.eql(mockMsgReceipt);
+      helper.removeMsgReceipt(mockMsgId, function (err) {
+        should.not.exist(err);
+        done();
+      });
+    });
+  });
+
+  it('recycle msg recept', function (done) {
+    helper.recycle(function (err, data) {
+      should.not.exist(err);
+      done();
+    });
+  });
+});
